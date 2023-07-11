1b4f3134e6e82e0efa16476e3393bc2ade64eaf9
saisashreek <saisashreekd@gmail.com>
2023-07-11 15:14:09 +0530

    Chat Side Bar UI Completed

---
+++ src/pages/dashboard/Chats.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Box } from "@mui/material";
+
+const Chats = () => {
+  return (
+    <Box
+      sx={{
+        position: "relative",
+        height: "100%",
+        width: 320,
+        backgroundColor: "#F8FAFF",
+        boxShadow: "0px 0px 2px rgba(0, 0, 0, 0.25)",
+      }}></Box>
+  );
+};
+
+export default Chats;
